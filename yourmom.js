7cbc6dc39cf90f8849f24f4231ec201408d0ad40
delvedor <tommydelved@gmail.com>
2016-07-27 15:29:25 +0200

    Updated to new db

--- yourmom.js
+++ yourmom.js
@@ -3,20 +3,50 @@
 // Imports
 const TelegramBot = require('node-telegram-bot-api')
 const matchAll = require('match-all')
-const level = require('level')
+const db = require('./db')
+const remove = require('unordered-array-remove')
 
 // Declarations
 const { token, chatId } = require('./token.json')
-const bot = new TelegramBot(token, {polling: true})
-const db = level('.yourmomdb')
+let bot = null
+if (process.env.NODE_ENV === 'production') {
+  bot = new TelegramBot(token)
+  bot.setWebHook(process.env.HEROKU_URL + bot.token)
+} else {
+  bot = new TelegramBot(token, { polling: true })
+}
+// Custom keyboards
+const keyboards = {
+  howMom: {
+    reply_markup: JSON.stringify({
+      keyboard: [
+        [{ text: 'Bene' }],
+        [{ text: 'Male' }]
+      ]
+    })
+  },
+  hideKeyboard: {
+    reply_to_message_id: null,
+    reply_markup: JSON.stringify({
+      hide_keyboard: true,
+      selective: true
+    })
+  }
+}
 
-bot.onText(/\/how/, function (message, match) {
+// /mom command
+bot.onText(/\/mom/, function (message, match) {
   const reg = /@(\w+)/gi
   const users = matchAll(message.text, reg).toArray()
+  // If the user sends "/mom@Your_Mom_Bot"
+  const botName = users.indexOf('Your_Mom_Bot')
+  if (botName !== -1) remove(users, botName)
+  if (!users.length) return
+
   users.forEach((user) => {
     db.get(user.toLowerCase(), (err, value) => {
       if (err) {
-        if (err.notFound) {
+        if (err.error === 'Key not found') {
           bot.sendMessage(chatId, 'Non ha ancora risposto!')
         } else {
           console.log(err)
@@ -29,18 +59,25 @@ bot.onText(/\/how/, function (message, match) {
   })
 })
 
-const howMom = {
-  reply_markup: JSON.stringify({
-    keyboard: [
-      [{text: 'Bene', callback_data: '1'}],
-      [{text: 'Male', callback_data: '2'}]
-    ],
-    'one_time_keyboard': true
+// /allmom command
+bot.onText(/\/allmom/, function (message, match) {
+  db.all((err, iterator) => {
+    if (err) {
+      console.log(err)
+      bot.sendMessage(chatId, 'Ops, è accaduto qualcosa di spiacevole!')
+    }
+    for (let [key, value] of iterator()) {
+      bot.sendMessage(chatId, `${key} ${value}`)
+    }
   })
-}
+})
 
-setInterval(() => {
-  bot.sendMessage(chatId, 'Come sta la tua mamma?', howMom)
+// /mymom command
+bot.onText(/\/mymom/, sendHowMom)
+
+// Sends "How is your mom?" with a custom keyboard response.
+function sendHowMom (message, match) {
+  bot.sendMessage(chatId, 'Come sta la tua mamma?', keyboards.howMom)
     .then((message) => {
       bot.onReplyToMessage(message.chat.id, message.message_id, (reply) => {
         db.put(reply.from.username.toLowerCase(), reply.text, (err) => {
@@ -48,18 +85,12 @@ setInterval(() => {
             console.log(err)
             bot.sendMessage(chatId, 'Ops, è accaduto qualcosa di spiacevole!')
           }
+          keyboards.hideKeyboard.reply_to_message_id = reply.message_id
+          bot.sendMessage(chatId, 'ok!', keyboards.hideKeyboard)
         })
       })
     })
-}, 1000 * 60 * 60 * 24)
+}
 
-bot.onText(/\/howall/, function (message, match) {
-  db.createReadStream()
-    .on('data', (data) => {
-      bot.sendMessage(chatId, `${data.key} ${data.value}`)
-    })
-    .on('error', (err) => {
-      console.log(err)
-      bot.sendMessage(chatId, 'Ops, è accaduto qualcosa di spiacevole!')
-    })
-})
+sendHowMom()
+// setInterval(sendHowMom, 1000 * 60 * 60 * 24)
