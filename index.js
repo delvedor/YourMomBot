787c68c1a310d98eba76172981ed641a1b6ab4b6
delvedor <tommydelved@gmail.com>
2016-07-28 16:14:01 +0200

    Experiments

--- index.js
+++ index.js
@@ -3,10 +3,8 @@
 const http = require('http')
 setInterval(() => {
   http.get('https://yourmotherbot.herokuapp.com/')
-}, 1000 * 60 * 25)
+}, 1000 * 60 * 20)
 
-const { bot, sendHowMom } = require('./yourmom')
+const { bot } = require('./yourmom')
 require('./server')(bot)
-
-setInterval(sendHowMom, 1000 * 60 * 60 * 24)
 
